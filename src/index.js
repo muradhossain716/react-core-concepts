89c5a2b6e49d3a8f7f25bf6e010e3b9f58dde1cb
Murad <muradahamed6262@gmail.com>
2022-05-25 12:59:47 +0600

    using html within js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+const date=new Date();
+const year=date.getFullYear();
+const arr=[ "January", "February", "March", "April", "May", "June", 
+"July", "August", "September", "October", "November", "December" ];
+const month=arr[date.getMonth()]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<h1>Welcome</h1>
+  <div>
+    <h1>Welcome to {month} {year}</h1>
+  </div>
+
 );
 
