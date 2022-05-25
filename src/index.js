94675cda8dc54cc94921663215d8e5f6bfd45cbb
Murad <muradahamed6262@gmail.com>
2022-05-25 13:12:07 +0600

    month added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 const date=new Date();
 const year=date.getFullYear();
 const arr=[ "January", "February", "March", "April", "May", "June", 
 "July", "August", "September", "October", "November", "December" ];
 const month=arr[date.getMonth()]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
