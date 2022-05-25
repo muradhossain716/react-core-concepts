277810f64279252093bea50c6c5182dc7cbeeb8a
Murad <muradahamed6262@gmail.com>
2022-05-25 22:42:34 +0600

    data loaded from json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Card from './Components/Card'
-
+import cardData from './data.json'
+{/* <Card title='Rafan Ahmed' description="I am a Web Developer."/>
+        <Card title='Rakib Ahmed' description='I am a Junior Web Developer'/>  */}
 function App(){
     return <div>
-        <Card title='Rafan Ahmed' description="I am a Web Developer."/>
-        <Card title='Rakib Ahmed' description='I am a Junior Web Developer'/>
+         {
+             cardData.map((el,i)=><Card key={i} title={el.title} description={el.description}/>)
+
+         }
     </div>
 }
 export default App;
