2e0c3338c6f2c04e2b826eca02e6d35d31de7763
Murad <muradahamed6262@gmail.com>
2022-05-27 11:36:13 +0600

    header using bootstrap

--- src/Components/Card/Card3.js
+++ src/Components/Card/Card3.js
@@ -3,15 +3,18 @@ import Card from 'react-bootstrap/Card';
 
 export default function Card3(){
     return(
-    <Card  bg='warning' text="white" style={{width:'300px'}} className='mb-2'>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-        <Card.Title> Card Title </Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        </Card.Body>
-    </Card>
+    <div>
+        <h1 className="bg-primary" style={{width:"400px"}}>Card Using Bootstrap</h1>
+        <Card  bg='warning' text="white" style={{width:'300px'}} className='mb-2'>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+            <Card.Title> Card Title </Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>
     )
 }
