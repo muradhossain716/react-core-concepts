b58515d475738079e0076bec18b3898de06cd5bb
Murad <muradahamed6262@gmail.com>
2022-05-27 08:30:54 +0600

    how react create tags using react.createElement

--- src/Components/Card/Card2.js
+++ src/Components/Card/Card2.js
@@ -1,12 +1,21 @@
 import React,{Component} from "react";
 
+function HowReactCreateElement(){
+    return React.createElement("div",{},
+    React.createElement('p',{},'This is a text created using React.createElement.')
+    )
+}
 class Card2 extends Component{
     render(){
         return(
-        <div className="card">
-            <h1 className="card-title">{this.props.name}</h1>
-            <p className="card-desription">{this.props.description}</p>
-            </div>
+        <div>
+            <div className="card">
+                <h1 className="card-title">{this.props.name}</h1>
+                <p className="card-desription">{this.props.description}</p>
+                </div>
+
+                <HowReactCreateElement />
+        </div>
         )
     }
 }
