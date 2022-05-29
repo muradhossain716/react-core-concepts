36ba29a64b739e31cd1f4986452e3e7979e3f6ba
Murad <muradahamed6262@gmail.com>
2022-05-29 13:22:32 +0600

    using Css styles in js

--- src/Components/State/UseState2.js
+++ src/Components/State/UseState2.js
@@ -1,14 +1,19 @@
-import React,{useState}from 'react'
+import React,{useState}from 'react';
+import Style from './state.module.css';
 
 export default function USESTATE2() {
     const [count,setCount]=useState(0);
     const handleIncrement=()=>{
         setCount(count+1);
     }
+    const handleDecement=()=>{
+      setCount(count-1)
+    }
   return (
   <div>
-      <p>{count}</p>
+      <p className={Style.color}>{count}</p>
       <button onClick={handleIncrement}>increment</button>
+      <button onClick={handleDecement} disabled={count<=0?true:false}>decrement</button>
 
   </div>
       
